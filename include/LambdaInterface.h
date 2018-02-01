c5f28541ab9d09c4d7155da704a3ab1dbc605bfe
teresa <tnunez@mail.desy.de>
2018-02-01 12:59:57 +0100

    Adding lambda files. In development

---
+++ include/LambdaInterface.h
@@ -0,0 +1,54 @@
+#ifndef LAMBDAINTERFACE_H
+#define LAMBDAINTERFACE_H
+
+#include "lima/Debug.h"
+#include "LambdaCompatibility.h"
+#include "lima/HwInterface.h"
+#include "lima/HwBufferMgr.h"
+
+
+using namespace std;
+
+namespace lima
+{
+namespace Lambda
+{
+class Camera;
+class DetInfoCtrlObj;
+class SyncCtrlObj;
+/*******************************************************************
+ * \class Interface
+ * \brief Lambda hardware interface
+ *******************************************************************/
+
+class LIBLAMBDA_API Interface: public HwInterface
+{
+DEB_CLASS_NAMESPC(DebModCamera, "LambdaInterface", "Lambda");
+
+public:
+	Interface(Camera& cam);
+	virtual ~Interface();
+
+	//- From HwInterface
+	virtual void getCapList(CapList&) const;
+	virtual void reset(ResetLevel reset_level);
+	virtual void prepareAcq();
+	virtual void startAcq();
+	virtual void stopAcq();
+	virtual void getStatus(StatusType& status);
+	virtual int getNbHwAcquiredFrames();
+	
+    //! get the camera object to access it directly from client
+    Camera& getCamera() { return m_cam;}
+
+private:
+	Camera& m_cam;
+	CapList m_cap_list;
+	DetInfoCtrlObj* m_det_info;
+	SyncCtrlObj* m_sync;
+};
+
+} // namespace Lambda
+} // namespace lima
+
+#endif // LAMBDAINTERFACE_H
---
+++ src/LambdaDetInfoCtrlObj.cpp
@@ -0,0 +1,116 @@
+#include "LambdaCamera.h"
+#include "LambdaDetInfoCtrlObj.h"
+
+
+using namespace lima;
+using namespace lima::Lambda;
+
+/*******************************************************************
+ * \brief DetInfoCtrlObj constructor
+ *******************************************************************/
+DetInfoCtrlObj::DetInfoCtrlObj(Camera& cam) :	m_cam(cam)
+{
+	DEB_CONSTRUCTOR();
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+DetInfoCtrlObj::~DetInfoCtrlObj()
+{
+	DEB_DESTRUCTOR();
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+void DetInfoCtrlObj::getMaxImageSize(Size& size)
+{
+	DEB_MEMBER_FUNCT();
+	// get the max image size
+	getDetectorImageSize(size);
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+void DetInfoCtrlObj::getDetectorImageSize(Size& size)
+{
+	DEB_MEMBER_FUNCT();
+	// get the max image size of the detector
+	m_cam.getImageSize(size);
+
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+void DetInfoCtrlObj::getDefImageType(ImageType& image_type)
+{
+	DEB_MEMBER_FUNCT();
+	m_cam.getImageType(image_type);
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+void DetInfoCtrlObj::getCurrImageType(ImageType& image_type)
+{
+	DEB_MEMBER_FUNCT();
+	m_cam.getImageType(image_type);
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+void DetInfoCtrlObj::setCurrImageType(ImageType image_type)
+{
+	DEB_MEMBER_FUNCT();
+	m_cam.setImageType(image_type);
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+void DetInfoCtrlObj::getPixelSize(double& x_size, double& y_size)
+{
+	DEB_MEMBER_FUNCT();
+	x_size = y_size = 172.0;
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+void DetInfoCtrlObj::getDetectorType(std::string& type)
+{
+	DEB_MEMBER_FUNCT();
+	type = "Lambda";
+
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+void DetInfoCtrlObj::getDetectorModel(std::string& model)
+{
+	DEB_MEMBER_FUNCT();
+	m_cam.getDetectorModel(model);
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+void DetInfoCtrlObj::registerMaxImageSizeCallback(HwMaxImageSizeCallback& cb)
+{
+	//m_mis_cb_gen.registerMaxImageSizeCallback(cb);
+}
+
+//-----------------------------------------------------
+//
+//-----------------------------------------------------
+void DetInfoCtrlObj::unregisterMaxImageSizeCallback(HwMaxImageSizeCallback& cb)
+{
+	//m_mis_cb_gen.unregisterMaxImageSizeCallback(cb);
+}
+
+
